472a25face65acd4ac41c216b5f530eb1ed91f89
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2020-02-10 14:37:35 -0600

    Updating roxygen documentation

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -13,6 +13,13 @@
 #   Check Package:             'Cmd + Shift + E'
 #   Test Package:              'Cmd + Shift + T'
 
+
+#' @title Hello, Garbanzo!
+#' @description Your friendly garbanzo will say hi to you
+#' @examples
+#' hello()
+#'
+#' @export
 hello <- function() {
-  print("Hello, world!")
+  print("Hello, I'm your friendly garbanzo!")
 }
